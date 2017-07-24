ad179b8a5657adec7e0c4883330777b0bd4ed238
serena617 <sp09m@my.fsu.edu>
2017-07-24 08:32:29 -0400

    search by location improved

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+cityData = ["Alachua","Alford","Altamonte Springs","Altha","Alva","Anna Maria","Anthony","Apalachicola","Apopka","Arcadia","Archer","Astatula","Atlantic Beach","Atlantis","Auburndale","Aventura","Avon Park","Baker","Baldwin","Bal Harbour","Balm","Bartow","Bascom","Bay Harbor Islands","Bay Lake","Bell","Belleair","Belleair Beach","Belleiar Bluffs","Belleair Shore","Belle Glade","Belle Isle","Belleview","Beverly Beach","Biscayne Park","Blounstown","Boca Raton","Bonifay","Bonita Springs","Bowling Green","Boynton Beach","Bradenton","Bradenton Beach","Brandon","Branford","Briny Breezes","Bristol","Bronson","Brooker","Brooksville","Bruce","Bryceville","Bunnell","Bushnell","Callahan","Callaway","Campbellton","Candler","Cantonment","Cape Canaveral","Cape Coral","Carrabelle","Caryville","Casselberry","Cedar Grove","Cedar Key","Center Hill","Century","Chattahoochee","Chiefland","Chipley","Chuluota","Cinco Bayou","Clearwater","Clemont","Clewiston","Cloud Lake","Cocoa","Cocoa Beach","Coconut Creek","Coconut Grove","Coleman","Cooper City","Coral Gables","Coral Springs","Cottondale","Crawfordville","Crescent City","Crestview","Cross City","Crystal River","Dade City","Dania Beach","Davenport","Davie","Daytona Beach","Daytona Beach Shores","DeBary","Deerfield Beach","DeFuniak Springs","Deland","Deleon Springs ","Delray Beach","Deltona","Destin","Doral","Dover","Dundee","Dunedin","Dunnellon","Durant","Eagle Lake","Eatonville","Ebro","Edgewater","Edgewood","Elkton","El Portal","Englewood","Eustis","Everglades City","Fanning Springs","Felda","Fellenfere","Fellsmere","Fernandina Beach","Fern Park","Flagler Beach","Florahome","Floral City","Florida City","Fort Lauderdale","Fort McCoy","Fort Meade","Fort Myers","Fort Myers Beach","Fort Pierce","Fort Walton Beach","Fort White","Fountain","Freeport","Frostproof","Fruitland Park","Gainesville","Georgetown","Gibsonton","Glen Ridge","Glen St. Mary","Golden Beach","Golf","Graceville","Grand Ridge","Grant","Greenacres","Green Cove Springs","Greensboro","Greenville","Greenwood","Gretna","Groveland","Gulf Breeze","Gulfport","Gulf Stream","Haines City","Hallandale Beach","Hampton","Hastings","Havana","Haverhill","Hawthorne","Hernando","Hialeah","Hialeah Gardens","High Springs","Highland Beach","Highland Park","Hilliard","Hillcrest Heights","Hillsboro Beach","Holiday","Holly Hill","Hollywood","Holmes Beach","Homestead","Homosassa","Horseshoe Beach","Howey-in-the-Hills","Hudson","Hypoluxo","Indialantic","Indian Creek","Indian Harbour Beach","Indian River Shores","Indian Rocks Beach","Indian Shores","Indiantown","Inglis","Interlachen","Inverness","Islandia","Istachatta","Jacob City","Jacksonville","Jacksonville Beach","Jasper","Jay","Jennings","Jensen Beach","Juno Beach","Jupiter","Jupiter Inlet Colony","Jupiter Island","Kathleen","Kenneth City","Key Biscayne","Key Colony Beach","Key West","Keystone Heights","Kissimmee","LaBelle","La Crosse","Lady Lake","Lake Alfred","Lake Buena Vista","Lake Butler","Lake City","Lake Clarke Shores","Lake Hamilton","Lake Helen","Lake Mary","Lake Monroe","Lake Park","Lake Placid","Lake Suzy","Lake Wales","Lake Worth","Lakeland","Land O’ Lakes","Lantana","Largo","Lauderdale Lakes","Lauderdale by the Sea","Lauderhill","Laurel Hill","Lawtey","Layton","Lazy Lake","Lecanto","Lee","Leesburg","Lehigh","Lehigh Acres","Lighthouse Point","Lithia","Live Oak","Longboat Key","Longwood","Loxahatchee","Lutz","Lynn Haven","Macclenny","Madeira Beach","Madison","Maitland","Malabar","Malone","Manalapan","Mangonia Park","Marathon","Marco Island","Margate","Marianna","Mary Esther","Mascotte","Mayo","McDavid","McIntosh","Medley","Melbourne","Melbourne Beach","Melbourne Village","Melrose","Merritt Island","Mexico Beach","Miami","Miami Beach","Miami Lakes","Miami Shores Village","Miami Springs","Micanopy","Middleburg","Midway","Milton","Minneola","Miramar","Monticello","Montiverde","Moore Haven","Murdock","Mount Dora","Mulberry","Myakka","Naples","Navarre","Neptune Beach","Newberry","New Port Richey","New Smyrna Beach","Niceville","Nobleton","Nokomis","Noma","North Bay Village","North Lauderdale","North Miami","North Miami Beach","North Palm Beach","North Port","North Redington Beach","Oak Hill","Oakland","Oakland Park","O’Brien","Ocala","Ocean Breeze Park","Ocean Ridge","Ocoee","Odessa","Okeechobee","Oldsmar","Opa-Locka","Orange City","Orange Park","Orchid","Orlando","Ormond Beach","Osprey","Osteen","Otter Creek","Oviedo","Ozona","Pace","Pahokee","Palatka","Palm Bay","Palm Beach","Palm Beach Gardens","Palm Beach Shores","Palm Coast","Palm Harbor","Palm Shores","Palm Springs","Palmetto","Panama City","Panama City Beach","Parker","Parkland","Parrish","Paxton","Pembroke Park","Pembroke Pines","Penney Farms","Pensacola","Perry","Pierson","Pinecrest","Pinellas Park","Plantation","Plant City","Polk City","Pomona Park","Pompano Beach","Ponce De Leon","Ponce Inlet","Port Charlotte","Port Orange","Port Richey","Port St. Joe","Port St. Lucie","Punta Gorda","Quincy","Raiford","Reddick","Redington Beach","Redington Shores","Riverview","Riviera Beach","Rockledge","Royal Palm Beach","Safety Harbor","San Antonio","Sanderson","Sanford","Sanibel","Sarasota","Satellite Beach","Sea Ranch Lakes","Sebastian","Sebring","Seminole","Sewall's Point","Shalimar","Silver Springs","Sneads","Sopchoppy","South Bay","South Daytona","South Miami","South Palm Beach","South Pasadena","Southport","Southwest Ranches","Springfield","Spring Hill","St. Augustine","St. Augustine Beach","St. Cloud","St. Leo","St. Lucie Village","St. Marks","St. Pete Beach","St. Petersburg","Starke","Stuart","Summerfield","Sunny Isles Beach","Sunrise","Surfside","Sweetwater","Tallahassee","Tamarac","Tampa","Tarpon Springs","Tavares","Telogia","Temple Terrace","Tequesta","Thonotosassa","Titusville","Treasure Island","Trenton","Umatilla","Valparaiso","Valrico","Venice","Vernon","Vero Beach","Virginia Gardens","Waldo","Wauchula","Wausau","Webster","Weeki Wachee","Weirsdale","Welaka","Wellington","Wesley Chapel","West Melbourne","West Miami","West Palm Beach","Weston","Westville","Wewahitchka","White Springs","Wildwood","Williston","Wilton Manors","Windemere","Winter Garden","Winter Haven","Winter Park","Winter Springs","Worthington Springs","Yankeetown","Yulee","Zephyrhills","Zolfo Springs"]
+
 //
 // Form functions
 //
@@ -5,6 +7,14 @@ $('#trailType li a').click(function(){
 	$('#trailType .btn:first-child').html($(this).text() + " <span class='caret'></span>");
 	$('#trailType .btn:first-child').val($(this).text());
 })
+$('#inputLocation').autocomplete({
+	source: function(request, response){
+		var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( request.term ), "i" );
+			response( $.grep(cityData, function( item ){
+				return matcher.test( item );
+			}) );
+    }
+});
 
 //
 // Raster
@@ -60,32 +70,56 @@ var vector = new ol.layer.Vector({
 // Create map object
 //
 var map = new ol.Map({
-        layers: [raster, vector],
-        target: 'map',
-        controls: ol.control.defaults({
-          attributionOptions:({
-            collapsible: false
-          })
-        }),
-        view: new ol.View({
-		  center: ol.proj.fromLonLat([-83.96, 27.95]),
-		  zoom: 7
-        })
-      });
+	layers: [raster, vector],
+	target: 'map',
+	controls: ol.control.defaults({
+	  attributionOptions:({
+		collapsible: false
+	  })
+	}),
+	view: new ol.View({
+	  center: ol.proj.fromLonLat([-83.96, 27.95]),
+	  zoom: 7
+	})
+});
 	  
 //
+// Refocus map
 //
-//
-var geocoder;
-var map;
-function initialize() {
-	geocoder = new google.maps.Geocoder();
-	var latlng = new google.maps.LatLng(-34.397, 150.644);
-	var mapOptions = {
-	  zoom: 8,
-	  center: latlng
-	}
-	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+function refocusMap(coordinates){ // Coordinates in JSON
+	map.getView().setCenter(ol.proj.fromLonLat(
+		[coordinates.lng, coordinates.lat]));
+	map.getView().setZoom(11);
+	var extent = map.getView().calculateExtent(map.getSize())
+	console.log(extent)
+	console.log(ol.proj.Projection.getCode())
+	//console.log(ol.proj.transformExtent(extent))
 }
 
-initialize()
+	  
+//
+// Get lat long from search input
+//
+var geocoder = new google.maps.Geocoder();
+$('#mainSearchButton').click(function(){
+	initializeGeocoder(geocoder)
+})
+var address
+function initializeGeocoder(geocoder) {
+	var coordinates
+	address = $('#inputLocation').val() + ", FL";
+	console.log(address)
+	geocoder.geocode({'address': address}, function(results, status){
+		coordinates = 0
+		if (status === "OK"){
+			console.log("found " + address)
+			coordinates = results[0].geometry.location.toJSON()
+			refocusMap(coordinates)
+		}
+		else {
+			alert("Cannot find " + address)
+		}
+	});
+	return coordinates
+}
+
